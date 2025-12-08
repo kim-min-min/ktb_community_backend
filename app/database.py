612855924db6a8c1f56493c06ff37fec12b0e09e
Min <kaner0529@gmail.com>
2025-12-08 15:31:19 +0900

    AWS 배포

--- app/database.py
+++ app/database.py
@@ -1,18 +1,19 @@
-# app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:1q2w3e4r@127.0.0.1:3306/ktb_community?charset=utf8mb4"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True,   
+    echo=True,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
